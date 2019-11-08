79d746a1b3a9e47ef42b65157b235ca634e19393
centurions90 <chrisaaron.cheng@gmail.com>
2019-11-08 19:40:22 +0800

    added file

---
+++ mastermind.c
@@ -0,0 +1,28 @@
+/*
+	Name: Cheng, Chris Aaron L.
+	Terminal: 24
+	Section: S11
+*/
+
+#include <stdio.h>
+//#include <windows.h>
+#include <stdlib.h>
+
+int isValid(int nInput, int nStart, int nEnd) {
+	if (nInput >= nStart && nInput <= nEnd) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+int menu(int *nInput) {
+
+
+}
+
+int main() {
+
+
+	return 0;
+}
