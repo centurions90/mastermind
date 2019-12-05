796a57a55f43e4664ae869a42d46bcc21cde103b
centurions90 <chrisaaron.cheng@gmail.com>
2019-12-05 19:31:02 +0800

    added color functionality and finished displays for main menu

--- mastermind.c
+++ mastermind.c
@@ -7,11 +7,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <windows.h>
 
 #define MAIN 		0
 #define GAME 		1
 #define SETTINGS	2
 #define EXIT		3
+
+#define RED		BACKGROUND_RED | FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY
+#define GREEN	BACKGROUND_GREEN
+#define BLUE	BACKGROUND_BLUE
+#define YELLOW	BACKGROUND_RED | BACKGROUND_GREEN
+#define CYAN	BACKGROUND_GREEN | BACKGROUND_BLUE
+#define MAGENTA BACKGROUND_RED | BACKGROUND_BLUE
+#define BRED	BACKGROUND_RED | BACKGROUND_INTENSITY
+#define BGREEN	BACKGROUND_GREEN | BACKGROUND_INTENSITY
+#define BBLUE	BACKGROUND_BLUE | BACKGROUND_INTENSITY
+#define WHITE	BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE
 
 // stores choice and returns true if choice is in range
 int getInputInRange(int *nChoice, int nStart, int nEnd) {
@@ -29,27 +41,136 @@ void clrscr() {
 	system("cls || clear");
 }
 
-// displays graphical interface of main menu
-void displayMain() {
+int generateRandomCode() {
+	int number = rand() % 10000;
+}
 
+// displays graphical interface of main menu
+void displayMain(HANDLE hConsole, WORD saved_attributes) {
+	SetConsoleTextAttribute(hConsole, RED);
+	printf("XX XX  XX   XXX XXXX XXXX XXX  XX XX XXXX X  X XXX \n");
+	printf("XX XX X  X X     XX  Xxx  X  X XX XX  XX  XX X X  X\n");
+	printf("X X X XXXX   XX  XX  X    XXX  X X X  XX  X XX X  X\n");
+	printf("X   X X  X XXXX  XX  XXXX X  X X   X XXXX X  X XXX \n\n");
+
+	SetConsoleTextAttribute(hConsole, saved_attributes);
+	printf("\t\t1 - Play\n");
+	printf("\t\t2 - Settings\n");
+	printf("\t\t3 - Exit\n\n");
+	printf("Enter number here: ");
 }
 
 // displays graphical interface of settings
-void displaySettings() {
+void displaySettings(HANDLE hConsole, WORD saved_attributes) {
+	SetConsoleTextAttribute(hConsole, RED);
+	printf("Settings\n\n");
+
+	SetConsoleTextAttribute(hConsole, saved_attributes);
+	printf("\t1 - Change number of guesses\n");
+	printf("\t2 - Change number of players\n");
+	printf("\t3 - Change assigned colors\n");
+	printf("\t4 - Back\n\n");
+	printf("Enter number here: ");
+}
+
+void displayGame() {
 
 }
 
-// initialize random number generator
-void initialize() {
+void constructor() {
 	srand(time(0));
+	system("color 8f");
+	clrscr();
 }
 
-int checkExactMatches(int nCode, int nGuess, int* nMatch, int* nCorrect) {
+void destructor() {
+	system("color 07");
+	clrscr();
+}
+
+int checkExactMatches(int nCode, int nGuess) {
 
 }
 
-int checkCorrectDigits() {
+int checkCorrectDigits(int nCode, int nGuess) {
 
+}
+
+void assignColor(
+	int num,
+	int nColor,
+	int* nColor1,
+	int* nColor2,
+	int* nColor3,
+	int* nColor4,
+	int* nColor5,
+	int* nColor6,
+	int* nColor7,
+	int* nColor8,
+	int* nColor9
+) {
+	switch (num) {
+		case 1:
+			*nColor1 = nColor;
+			break;
+		case 2:
+			*nColor2 = nColor;
+			break;
+		case 3:
+			*nColor3 = nColor;
+			break;
+		case 4:
+			*nColor4 = nColor;
+			break;
+		case 5:
+			*nColor5 = nColor;
+			break;
+		case 6:
+			*nColor6 = nColor;
+			break;
+		case 7:
+			*nColor7 = nColor;
+			break;
+		case 8:
+			*nColor8 = nColor;
+			break;
+		case 9:
+			*nColor9 = nColor;
+	}
+}
+
+int getColor(
+	int num,
+	int* nColor1,
+	int* nColor2,
+	int* nColor3,
+	int* nColor4,
+	int* nColor5,
+	int* nColor6,
+	int* nColor7,
+	int* nColor8,
+	int* nColor9
+) {
+	switch (num) {
+		case 1:
+			return *nColor1;
+		case 2:
+			return *nColor2;
+		case 3:
+			return *nColor3;
+		case 4:
+			return *nColor4;
+		case 5:
+			return *nColor5;
+		case 6:
+			return *nColor6;
+		case 7:
+			return *nColor7;
+		case 8:
+			return *nColor8;
+		case 9:
+			return *nColor9;
+	}
 }
 
 int main() {
@@ -57,14 +178,35 @@ int main() {
 	int nGuesses	= 10;
 	int nPlayers	= 2;
 	int nMenu		= 0;
+	int nMatch;
+	int nCorrect;
+
+	int nColor1 = RED;
+	int nColor2 = GREEN;
+	int nColor3 = BLUE;
+	int nColor4 = YELLOW;
+	int nColor5 = CYAN;
+	int nColor6 = MAGENTA;
+	int nColor7 = BRED;
+	int nColor8 = BGREEN;
+	int nColor9 = BBLUE;
+
+	constructor();
+
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
+    WORD saved_attributes;
+
+    GetConsoleScreenBufferInfo(hConsole, &consoleInfo);
+    saved_attributes = consoleInfo.wAttributes;
 
 	while (nMenu != EXIT) {
 		// main menu
 		if (nMenu == MAIN) {
-			displayMain();
+			displayMain(hConsole, saved_attributes);
 			while (!getInputInRange(&nChoice, 1, 3)) {
 				clrscr();
-				displayMain();
+				displayMain(hConsole, saved_attributes);
 				printf("Input Invalid. Try Again:\n");
 			}
 
@@ -79,10 +221,10 @@ int main() {
 
 		// settings
 		else if (nMenu == SETTINGS) {
-			displaySettings();
-			while (getInputInRange(&nChoice, 1, 4)) {
+			displaySettings(hConsole, saved_attributes);
+			while (!getInputInRange(&nChoice, 1, 4)) {
 				clrscr();
-				displaySettings();
+				displaySettings(hConsole, saved_attributes);
 				printf("Input Invalid. Try Again:\n");
 			}
 
@@ -112,9 +254,9 @@ int main() {
 
 		else if (nMenu == 6) {
 			nMenu = SETTINGS;
-			clrscr();
 		}
 	}
 
+	destructor();
 	return 0;
 }
